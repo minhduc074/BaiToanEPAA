c3e289dfa9280ae435ee15158e2fe504b31d1c2e
minhduc074 <minhduc074@gmail.com>
2018-07-31 23:30:43 +0700

    loadding screen

---
+++ BaiToan/loading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiToan
+{
+    public partial class loading : Form
+    {
+        public loading()
+        {
+            InitializeComponent();
+        }
+
+        void wait(int x)
+        {
+            DateTime t = DateTime.Now;
+            DateTime tf = DateTime.Now.AddSeconds(x);
+
+            while (t < tf)
+            {
+                t = DateTime.Now;
+            }
+            Form frm = new Form1();
+            frm.Show();
+        }
+
+        private void loading_Load(object sender, EventArgs e)
+        {
+            wait(5);
+        }
+    }
+}
